6dead034a208097457787603ecc0796d811c4c1f
Jake McGrath <thejakezilla80@gmail.com>
2023-10-12 19:18:45 -0500

    Created two BASH scripts.

---
+++ src/BASH_Scripts/bash_variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# bash_variables.sh
+
+my_var="Hello, world!"
+my_name="ob1jakobi"
+
+echo "$my_var"
+echo $my_name
---
+++ src/BASH_Scripts/echo_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# echo_test.sh
+
+echo "Hello, world!"
